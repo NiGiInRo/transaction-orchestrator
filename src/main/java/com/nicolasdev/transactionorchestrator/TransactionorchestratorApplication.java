39ef684fe6e58b9cc50178bde8e22e6c94115376
unknown <n.infanter@uniandes.edu.co>
2026-05-26 00:39:19 -0500

    feat: configuracion inicial del proyecto

---
+++ src/main/java/com/nicolasdev/transactionorchestrator/TransactionorchestratorApplication.java
@@ -0,0 +1,13 @@
+package com.nicolasdev.transactionorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionorchestratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionorchestratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nicolasdev/transactionorchestrator/TransactionorchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nicolasdev.transactionorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionorchestratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
